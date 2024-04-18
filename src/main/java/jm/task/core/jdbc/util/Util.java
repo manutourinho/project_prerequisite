7d6cf28e8ccecc2796605309bb5b324d70c0ad21
Manuela <manuelatourinhodeoliveira@gmail.com>
2024-04-18 02:09:28 -0300

    updating url variable in Util class to match new schema's name

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,7 +3,7 @@ package jm.task.core.jdbc.util;
 import java.sql.*;
 
 public class Util {
-    private final String url = "jdbc:mysql://localhost:3306/task_1_2";
+    private final String url = "jdbc:mysql://localhost:3306/project_prerequisite";
     private final String username = "devuser";
     private final String password = "Uvaroxa18*";
 
