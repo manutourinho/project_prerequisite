b82625c68b758f84bb4c09bf3c3cb18193165224
Manuela <manuelatourinhodeoliveira@gmail.com>
2024-04-25 19:14:09 -0300

    - updated the AnimalCage class; - ensured that the Animal and Timer dependencies are automatically injected by Spring by using the autowired annotation; - created Dog class; - specifyed the use of the dog class by the Animal dependency by using the qualifier annotation; - setting the beans in AppConfig class

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,24 @@
 package app.config;
 
+import app.model.AnimalsCage;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+    @Bean       // (name = "timer")
+    public Timer getTimer() {
+        return new Timer();
+
+    }
+
+    @Bean
+    public AnimalsCage animalsCage() {
+        return new AnimalsCage();
+    }
+
 }
